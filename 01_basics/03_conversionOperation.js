d9785f5e14b3f50beaefa7efdf7d867f83b0bc31
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-07-20 19:00:33 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,8 +34,47 @@ console.log(booleanIsString) //false
 // "" => false;
 // "hitesh" => true
 
-
+// String Conversion
 let someNummber = 33
 let stringNumber = String(someNummber)
 console.log(stringNumber);
 console.log(typeof stringNumber)
+
+// ********************************  Operations ********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);  //-3
+
+console.log(2+2);   
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); //power
+console.log(2/2);
+console.log(2%2); //remainder
+
+// Addition Of Two String
+let str1 = "Hello"
+let str2 = " Tanisha"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);       // 12
+console.log(1 + "2");       // 12
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
+
+// Tricky Conversion
+
+console.log(+true)     // Gives 1
+// console.log(true+) // Gives Error
+
+console.log(+"")     // Gives 0
+
+// Prefix Postfix
+let dist = 100
+++dist             // Prefix
+console.log(dist)  // 101
+dist++             // Postfix
+console.log(dist)  // 102
