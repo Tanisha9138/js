ae8015dc22fb42aacd65669083ff7288465c7f44
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-07-24 18:22:13 +0000

    more details about functions

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -18,3 +18,34 @@ function calculatedPrice2(val1,val2,...num1){
     return num1
 }
 console.log(calculatedPrice2(200,400,500,2000)); // [500,2000]
+
+// We can pass object in function
+const user = {
+    username: "Tanisha",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)   Username is Tanisha and price is 199
+
+// We can pass direct object as well
+handleObject({
+    username: "Harshu",
+    price: 399
+})
+
+// We can pass array as well
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray)); // 400
+
+// We can pass direct array as well
+console.log(returnSecondValue([200, 400, 500, 1000]));
