8494ad8c810a0c1075c2a520c8148dce1ebb4c83
Tanisha9138 <152298980+Tanisha9138@users.noreply.github.com>
2025-08-05 17:41:25 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -58,7 +58,7 @@ const promiseFour = new Promise(function(resolve,reject){
 
 // now we consume promiseFour
 
-// in this we use concept of chaining which means the value returned from above .then that value come in this .then 
+// in this we use concept of chaining/channable/thenable which means the value returned from above .then that value come in this .then 
 promiseFour.then((user) => {
     console.log(user);
     return user.username
@@ -70,3 +70,55 @@ promiseFour.then((user) => {
     console.log(error);
 })
 .finally(() => console.log("The promise is either resolved or rejected"))
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    },1000)
+})
+
+// Now we use async/await concept instead of .then
+// it waits a little for work if work happens,then only it moves forward otherwise it gives us an error, in this we can't handle a catch gracefully
+// problem of async is it cannot handle error directly
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response)
+    }catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+//
+async function getAllUsers(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        // console.log(response);
+        const data = await response.json() // response ko bhi convert hone me time lgta hai tho yha bhi await krana pdega
+        console.log(data);
+    }catch(error){
+        console.log("E: ", error);
+    }
+}
+getAllUsers()
+
+// if we do above work with .then 
+fetch('https://api.github.com/users/Tanisha9138')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
+// isme fetch phle kaam kr rha hai or jo setTimeout hmne ye functions likhe the ye baad me.
